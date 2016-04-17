5b230c6079cde1fa0f5151120a986bd4e4319fa4
JuzTosS <juztoss@gmail.com>
2016-04-17 14:44:58 +0300

    Added blank android app

---
+++ app/src/main/java/juztoss/com/bpmplayer/BPMPlayerApp.java
@@ -0,0 +1,25 @@
+package juztoss.com.bpmplayer;
+
+import android.app.Application;
+import android.content.res.Configuration;
+
+/**
+ * Created by JuzTosS on 4/16/2016.
+ */
+public class BPMPlayerApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+}
