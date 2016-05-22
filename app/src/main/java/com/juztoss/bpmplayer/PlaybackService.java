9d8c455bebcc59badb267f467b93c50dde3e1ffb
JuzTosS <juztoss@gmail.com>
2016-05-22 22:31:30 +0300

    Moved to Superpowered lib

--- app/src/main/java/com/juztoss/bpmplayer/PlaybackService.java
+++ app/src/main/java/com/juztoss/bpmplayer/PlaybackService.java
@@ -5,34 +5,34 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
-import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
-
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 
 import com.juztoss.bpmplayer.models.Playlist;
 import com.juztoss.bpmplayer.models.Song;
 import com.juztoss.bpmplayer.presenters.BPMPlayerApp;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 
 /**
  * Created by JuzTosS on 5/3/2016.
  */
-public class PlaybackService extends Service
+public class PlaybackService extends Service implements AdvancedMediaPlayer.AdvancedMediaPlayerListener
 {
 
     public static final int NOTIFICATION_ID = 42;
     public static final String LAUNCH_NOW_PLAYING_ACTION = "com.juztoss.bpmplayer.action.LAUNCH_NOW_PLAYING";
     public static final String SWITCH_PLAYBACK_ACTION = "com.juztoss.bpmplayer.action.SWITCH_PLAYBACK";
     public static final String UPDATE_UI_ACTION = "com.juztoss.bpmplayer.action.UPDATE_UI";
+    private static final String DEFAULT_SAMPLE_RATE = "44100";
+    private static final String DEFAULT_BUFFER_SIZE = "512";
 
-    private MediaPlayer mPlayer;
+    private AdvancedMediaPlayer mPlayer;
     private BPMPlayerApp mApp;
     private PlaybackService mSelf = this;
     private Playlist mPlaylist = new Playlist();
@@ -41,33 +41,26 @@ public class PlaybackService extends Service
 
     private BaseAction mActionInProgress;
     private boolean mIsPlaying = false;
-    private MediaPlayer.OnErrorListener mOnErrorListener = new MediaPlayer.OnErrorListener()
+
+    @Override
+    public void onEnd()
     {
-        @Override
-        public boolean onError(MediaPlayer mp, int what, int extra)
-        {
-            Log.e(this.getClass().toString(), "MEDIA PLAYER ERROR! " + what + ":" + extra);
-            return true;
-        }
-    };
-    private MediaPlayer.OnCompletionListener mOnCompletionListener = new MediaPlayer.OnCompletionListener()
+        gotoNext();
+    }
+
+    @Override
+    public void onError(String message)
     {
-        @Override
-        public void onCompletion(MediaPlayer mp)
-        {
-            gotoNext();
-        }
-    };
-    private boolean mIsPrepared = false;
+
+    }
 
     public void gotoNext()
     {
         clearQueue();
         mCurrentSongIndex++;
-        if(mCurrentSongIndex >= mPlaylist.songs().size())
+        if (mCurrentSongIndex >= mPlaylist.songs().size())
             mCurrentSongIndex = mPlaylist.songs().size() - 1;
 
-        putAction(new ActionPrepare(mPlaylist.songs().get(mCurrentSongIndex)));
         putAction(new ActionPlay());
     }
 
@@ -75,10 +68,9 @@ public class PlaybackService extends Service
     {
         clearQueue();
         mCurrentSongIndex--;
-        if(mCurrentSongIndex < 0)
+        if (mCurrentSongIndex < 0)
             mCurrentSongIndex = 0;
 
-        putAction(new ActionPrepare(mPlaylist.songs().get(mCurrentSongIndex)));
         putAction(new ActionPlay());
     }
 
@@ -110,7 +102,6 @@ public class PlaybackService extends Service
 
         mActionInProgress = null;
 
-        mPlayer.reset();
     }
 
 
@@ -126,10 +117,31 @@ public class PlaybackService extends Service
     {
         mApp = (BPMPlayerApp) getApplicationContext();
         mApp.setPlaybackService(this);
-        mPlayer = new MediaPlayer();
-        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        mPlayer.setOnErrorListener(mOnErrorListener);
-        mPlayer.setOnCompletionListener(mOnCompletionListener);
+
+        System.loadLibrary("AdvancedMediaPlayer");
+
+        String sampleRateString = null, bufferSizeString = null;
+        if (Build.VERSION.SDK_INT >= 17) {
+            AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            sampleRateString = audioManager.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE);
+            bufferSizeString = audioManager.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER);
+        }
+        if (sampleRateString == null) sampleRateString = DEFAULT_SAMPLE_RATE;
+        if (bufferSizeString == null) bufferSizeString = DEFAULT_BUFFER_SIZE;
+
+        final int sampleRate = Integer.parseInt(sampleRateString);
+        final int bufferSize = Integer.parseInt(bufferSizeString);
+
+        final PlaybackService self = this;
+        Runnable process = new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                mPlayer = new AdvancedMediaPlayer(sampleRate, bufferSize, self);
+            }
+        };
+        new Thread(process).start();
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -142,9 +154,8 @@ public class PlaybackService extends Service
 
     public void setSource(int index)
     {
-        mCurrentSongIndex = index;
-        clearQueue();
-        putAction(new ActionPrepare(mPlaylist.songs().get(index)));
+        final Song song = mPlaylist.songs().get(index);
+        mPlayer.setSource(song.source().getAbsolutePath());
     }
 
     private void setIsPlaying(boolean value)
@@ -201,10 +212,7 @@ public class PlaybackService extends Service
 
     public int getCurrentPosition()
     {
-        if(mIsPrepared)
-            return mPlayer.getCurrentPosition();
-        else
-            return 0;
+        return mPlayer.getPosition();
     }
 
     public int getDuration()
@@ -214,8 +222,7 @@ public class PlaybackService extends Service
 
     public void seekTo(int position)
     {
-        if(mIsPrepared)
-            mPlayer.seekTo(position);
+        mPlayer.setPosition(position);
     }
 
     private class BaseAction
@@ -243,44 +250,6 @@ public class PlaybackService extends Service
         }
     }
 
-    class ActionPrepare extends BaseAction
-    {
-
-        private MediaPlayer.OnPreparedListener mOnPrepared = new MediaPlayer.OnPreparedListener()
-        {
-            @Override
-            public void onPrepared(MediaPlayer mp)
-            {
-                mIsPrepared = true;
-                doNext();
-            }
-        };
-
-        private Song mSong;
-
-        public ActionPrepare(Song song)
-        {
-            mSong = song;
-        }
-
-        @Override
-        public void doAction()
-        {
-            setIsPlaying(true);
-            try
-            {
-                mIsPrepared = false;
-                mPlayer.setOnPreparedListener(mOnPrepared);
-                mPlayer.setDataSource(mSelf, android.net.Uri.fromFile(mSong.source()));
-                mPlayer.prepareAsync();
-            }
-            catch (IOException e)
-            {
-                Log.e(this.getClass().toString(), e.toString());
-            }
-        }
-    }
-
     class ActionPlay extends BaseAction
     {
         @Override
@@ -289,7 +258,7 @@ public class PlaybackService extends Service
             setIsPlaying(true);
             try
             {
-                mPlayer.start();
+                mPlayer.play();
             }
             catch (IllegalStateException e)
             {
@@ -321,9 +290,8 @@ public class PlaybackService extends Service
         public void doAction()
         {
             setIsPlaying(false);
-            mPlayer.stop();
-            mPlayer.reset();
-            mIsPrepared = false;
+            mPlayer.pause();
+            mPlayer.setPosition(0);
             updateUI();
             doNext();
         }
