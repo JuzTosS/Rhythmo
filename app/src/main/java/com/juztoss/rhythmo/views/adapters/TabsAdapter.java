dbde0f5b5b1ad29b635801bf5c2b532c7d77664c
JuzTosS <juztoss@gmail.com>
2017-01-22 15:35:17 +0300

    Removed unused imports

--- app/src/main/java/com/juztoss/rhythmo/views/adapters/TabsAdapter.java
+++ app/src/main/java/com/juztoss/rhythmo/views/adapters/TabsAdapter.java
@@ -4,7 +4,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.ViewGroup;
 
 import com.juztoss.rhythmo.views.fragments.PlaylistFragment;
