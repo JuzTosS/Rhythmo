5024ac589dfd8c36f375e6e5807e86d81494cd8e
JuzTosS <juztoss@gmail.com>
2017-02-19 22:48:03 +0300

    wip

--- app/src/main/jni/BPMDetector.cpp
+++ app/src/main/jni/BPMDetector.cpp
@@ -4,15 +4,36 @@
 #include <android/log.h>
 #include <malloc.h>
 #include <SuperpoweredSimple.h>
+#include <sys/stat.h>
+
+long getFileSize(const char *filename) {
+    struct stat st;
+
+    if (stat(filename, &st) == 0)
+        return st.st_size;
+
+    return -1;
+}
+
+//TODO: decrease memory consumption for each song.
+//1. Detect the file size
+//2. Detect the file length in bytes
+//3. Get the file size that enough for the detection
+//4. Read the file from the middle
 
 extern "C" JNIEXPORT jdouble Java_com_juztoss_rhythmo_audio_BpmDetector_detect(JNIEnv *env,
-                                                                              jobject instance,
-                                                                              jstring source
-                                                                              ) {
+                                                                               jobject instance,
+                                                                               jstring source
+) {
+
+    const bool DEBUG = false;
 
     const char *path = env->GetStringUTFChars(source, JNI_FALSE);
+    if(DEBUG) __android_log_print(ANDROID_LOG_ERROR, __func__, " Start decode, path %s", path);
+
     SuperpoweredDecoder *decoder = new SuperpoweredDecoder();
-    const char *openError = decoder->open(path, false, 0, 0);
+    const char *openError = decoder->open(path, true, 0, 0);
+    if(DEBUG) __android_log_print(ANDROID_LOG_ERROR, __func__, " Start decode, path %ld; %f; %i", decoder->durationSamples, decoder->durationSeconds, decoder->samplesPerFrame);
 
     if (openError) {
         __android_log_print(ANDROID_LOG_ERROR, __func__, " Decoder error, path %s, error: %s", path, openError);
@@ -20,20 +41,48 @@ extern "C" JNIEXPORT jdouble Java_com_juztoss_rhythmo_audio_BpmDetector_detect(J
         env->ReleaseStringUTFChars(source, path);
         return -1;
     };
-    env->ReleaseStringUTFChars(source, path);
 
-    const int samplesToDetect = 2097152; //The pow of 2 in case the fourier transform inside of the detector need it
-    int64_t duration = decoder->durationSamples;
-    int64_t start = duration / 2 - samplesToDetect / 2;
-    int64_t end = start + samplesToDetect;
+    double durationSeconds = decoder->durationSeconds;
+    delete decoder;
 
-    if(start <= 0)
+    long fileSize = getFileSize(path);
+    if(DEBUG) __android_log_print(ANDROID_LOG_ERROR, __func__, " File size: %ld", fileSize);
+
+    if(durationSeconds <= 0 || fileSize <= 0)
     {
-        start = 0;
-        end = decoder->durationSamples;
+        __android_log_print(ANDROID_LOG_ERROR, __func__, " Empty file, path %s", path);
+        env->ReleaseStringUTFChars(source, path);
+        return -1;
     }
 
-    decoder->seekTo(start, false);
+
+    const int minSecondsToDetect = 5;
+    double bytesPerSecond = (float)fileSize / durationSeconds;
+
+    int readingOffset = 0;
+    int readingLength = int(minSecondsToDetect * bytesPerSecond);
+
+    if(durationSeconds > minSecondsToDetect)
+    {
+        readingOffset = int(fileSize / 2) - readingLength / 2;
+    }
+    else
+    {
+        readingLength = 0;
+    }
+
+    decoder = new SuperpoweredDecoder();
+    if(DEBUG) __android_log_print(ANDROID_LOG_ERROR, __func__, " Open with: %d, %d", readingOffset, readingLength);
+    openError = decoder->open(path, false, readingOffset, readingLength);
+    if (openError) {
+        __android_log_print(ANDROID_LOG_ERROR, __func__, " Decoder error (2), path %s, error: %s", path, openError);
+        delete decoder;
+        env->ReleaseStringUTFChars(source, path);
+        return -1;
+    };
+
+    env->ReleaseStringUTFChars(source, path);
+
     SuperpoweredOfflineAnalyzer * analyzer = new SuperpoweredOfflineAnalyzer(decoder->samplerate, 0, decoder->durationSeconds);
 
     // Create a buffer for the 16-bit integer samples coming from the decoder.
@@ -42,7 +91,7 @@ extern "C" JNIEXPORT jdouble Java_com_juztoss_rhythmo_audio_BpmDetector_detect(J
     float *floatBuffer = (float *)malloc(decoder->samplesPerFrame * 2 * sizeof(float) + 1024);
 
     // Processing.
-    while (decoder->samplePosition <= end) {
+    while (decoder->samplePosition <= decoder->durationSamples) {
         // Decode one frame. samplesDecoded will be overwritten with the actual decoded number of samples.
         unsigned int samplesDecoded = decoder->samplesPerFrame;
         if (decoder->decode(intBuffer, &samplesDecoded) == SUPERPOWEREDDECODER_ERROR) break;
